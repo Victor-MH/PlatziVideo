7042f9d416d36ca23e7b4bc19f2b264616b5d874
Vico-kali <victormu497@gmail.com>
2021-08-26 21:16:48 -0500

    Clase 7 completada, integración de webpack con express, cuidado con la carpeta assets/static/ al descargar de github

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
+/* eslint-disable global-require */
 import express from 'express';
 import dotenv from 'dotenv';
+import webpack from 'webpack';
 
 dotenv.config();
 
@@ -7,15 +9,25 @@ const { ENV, PORT } = process.env;
 
 const app = express();
 
-if(ENV =='development' ) {
-    console.log('Development config');
+// eslint-disable-next-line space-infix-ops
+if (ENV ==='development') {
+  console.log('Development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+
+  const compiler = webpack(webpackConfig);
+  const serverConfig = { publicPath: webpackConfig.output.path, serverSideRender: true };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
 }
 
 app.get('*', (req, res) => {
-    res.send({ hello: 'express' });
+  res.send({ hello: 'express' });
 });
 
-app.listen(PORT, err => {
-    if(err) console.log(err);
-    else console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  else console.log(`Server running on port ${PORT}`);
+});
